0069a58e684f277cdc5f533abccb8092027abfc4
JawaraGordon <jawaragordon@icloud.com>
2022-05-22 22:38:26 -0400

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { movies } from "../data";
+import React from 'react';
+import { movies } from '../data';
+
+console.log(movies);
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
